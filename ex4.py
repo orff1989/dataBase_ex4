1aa54be551377bb7e6af741fc380b399c3676474
Or <orff1989@walla.com>
2022-06-06 18:16:04 +0300

    v2

--- ex4.py
+++ ex4.py
@@ -4,9 +4,10 @@ from pyspark.sql import Row
 
 sqlContext = SQLContext(sc)
 
+########### A #########
+
 df = sqlContext.read.option("multiline","true") \
       .json("books.json").drop(*('country', 'imageLink','language','link','pages'))
-
 
 t=df.rdd.filter(lambda r: r[0][0]=="F").map(lambda r: Row(r[0],r[1],2022-int(r[2]))).distinct()
 
@@ -16,4 +17,13 @@ a=sqlContext.createDataFrame(t,columns)
 a.printSchema()
 a.show()
 
+################ B ############
+df2 = sqlContext.read.option("multiline","true") \
+      .json("books.json").drop(*('country', 'imageLink','link','title','year'))
 
+t2=df2.rdd.filter(lambda r: r[1]=="English").map(lambda r: (r[0],int(r[2]))).reduceByKey(lambda a,b: a+b).collect()
+# t2=df2.rdd.filter(lambda r: r[1]=="English").collect()
+a2=sqlContext.createDataFrame(t2,['author', 'pages of english books'])
+
+a2.printSchema()
+a2.show(50)
